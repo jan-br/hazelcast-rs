e9de657b669df2d9d352463d9296065e5d205c2c
jan <contact@jan-br.de>
2022-12-13 02:53:43 +0100

    Add Serializer for i32

--- src/serialization/serializer/default/integer_serializer.rs
+++ src/serialization/serializer/default/integer_serializer.rs
@@ -1,10 +1,20 @@
+use std::mem::transmute;
+use std::sync::Arc;
 use crate::serialization::data::{DataInput, DataOutput};
 use crate::serialization::data::object_data_input::ObjectDataInput;
 use crate::serialization::data::object_data_output::ObjectDataOutput;
+use crate::serialization::serializable::Serializable;
 use crate::serialization::serializer::Serializer;
+use crate::serialization::service::SerializationServiceV1;
 
 #[derive(Default)]
 pub struct IntegerSerializer;
+
+impl Serializable for i32 {
+  fn get_serializer(&self, service: &SerializationServiceV1) -> Arc<dyn Serializer<Box<Self>>> {
+    unsafe { transmute(service.registry.get(service.serializer_name_to_id.get(&"integer".to_string()).unwrap()).unwrap().clone()) }
+  }
+}
 
 impl Serializer<Box<i32>> for IntegerSerializer {
   fn id(&self) -> i32 {
